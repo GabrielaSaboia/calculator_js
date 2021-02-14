8db47dab9d37ce80385d0d5ee33a71e80a751dfb
gabrielasaboia <gs525@njit.edu>
2021-02-14 09:07:21 -0500

    reformated the Calculator.js class, adding static methods and importing operations.

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,10 +1,43 @@
 //this line loads the library
 const MathOperations = require('./Operations/MathOperations');
+const Calculation = require('./models/Calculation');
+const Sum = require('./Operations/Addition');
+const Difference = require('./Operations/Subtraction');
+const Product = require('./Operations/Product');
+const Quatient = require('./Operations/Quotient');
+
 
 class Calculator{
-
- Sum(a,b){
-    return MathOperations.Sum(a,b);
-}
+    static Calculations = []
+    //Static methods can be called without instantiating
+    // and are good for actions
+    static Sum(a,b){
+     //this is how you create a new object and
+     // this is good for data and actions
+     let calculation = new Calculation(a,b,Sum);
+     Calculator.Calculations.push(calculation);
+    return calculation.GetResults();
+    }
+    static Difference(a,b) {
+        //this is how you create a new object and
+        // this is good for data and actions
+        let calculation = new Calculation(a,b,Difference);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Product(a,b) {
+        //this is how you create a new object and
+        // this is good for data and actions
+        let calculation = new calculation(a,b,Product);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
+    static Quotient(a,b){
+        //this is how you create a new object and
+        // this is good for data and actions
+        let calculation = new Calculation(a,b,Quotient);
+        Calculator.Calculations.push(calculation);
+        return calculation.GetResults();
+    }
 }
 module.exports = Calculator;
--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -1,12 +1,18 @@
 const Calculator = require('../src/Calculator');
+const Sum = require('../src/Operations/Addition');
+const Difference = require('../src/Operations/Subtraction');
+const Product = require('../src/Operations/Product');
+const Quatient = require('../src/Operations/Quotient');
 //Triple A testing = AAA
 //Arrange / We are loading the calculator
 
 test('Calculator adding two numbers', () =>{
     let calculator = new Calculator();
     //Act / We are running the code
-    let result = calculator.Sum( 1, 2);
+    let calculation = calculator.Sum( 1, 2);
 
     //Assert / We are asserting the result shopuld be 3
-    expect(result).toBe(3);
+    expect(calculation.a).toBe(1);
+    expect(calculation.b).toBe(2);
+    expect(calculation.op).toBe(op);
 });
