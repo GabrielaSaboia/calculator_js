f53a9002f00aaf983eb7fdfd75868125e9f4b327
gabrielasaboia <gs525@njit.edu>
2021-02-14 13:27:18 -0500

    trying to fix coveralls

--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -1,17 +1,13 @@
 const Calculator = require('../src/Calculator');
-const Sum = require('../src/Operations/Addition');
-const Difference = require('../src/Operations/Subtraction');
-const Product = require('../src/Operations/Product');
-const Quatient = require('../src/Operations/Quotient');
-//Triple A testing = AAA
-//Arrange / We are loading the calculator
+
+
 
 test('Calculator adding two numbers', () =>{
-    let calculator = new Calculator();
-    //Act / We are running the code
+    //assigning the results of the calc sum method to an
+    // object, calculator returns an object
+    let result = Calculator.Sum(1,2);
+   //expect is used to test if the results are correct
     let calculation = calculator.Sum( 1, 2);
-
-    //Assert / We are asserting the result shopuld be 3
     expect(calculation.a).toBe(1);
     expect(calculation.b).toBe(2);
     expect(calculation.op).toBe(op);
