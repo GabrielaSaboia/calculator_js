a9f1556989bea58d8b5181d1e6f25788de0d9706
gabrielasaboia <gs525@njit.edu>
2021-02-14 13:55:45 -0500

    Settup Calculator.test.js

--- tests/Calculator.test.js
+++ tests/Calculator.test.js
@@ -7,8 +7,29 @@ test('Calculator adding two numbers', () =>{
     // object, calculator returns an object
     let result = Calculator.Sum(1,2);
    //expect is used to test if the results are correct
-    let calculation = calculator.Sum( 1, 2);
-    expect(calculation.a).toBe(1);
-    expect(calculation.b).toBe(2);
-    expect(calculation.op).toBe(op);
+    expect(result).toBe(3);
+});
+
+test('Calculator difference between two numbers', () => {
+    let result = Calculator.Difference(1,2);
+    //expect is used to test if the result is correct
+    expect(result).toBe(-1);
+});
+
+test('Calculator product of two numbers', ()=>{
+    let result = Calculator.Product(1,2);
+
+    expect(result).toBe(2);
+});
+test('Calculator quotient of two numbers', ( => {
+    let result = Calculator.Quaotient(1,2);
+
+    expect(result).toBe(.5);
+}));
+test('Calculator adding to calculations', ()=>{
+    let result = Calculator.Quotient(1,2);
+    let calculations = Calculator.Calculations
+    calculations.forEach(function (calc) {
+        console.log(calc.GetResults())
+    });
 });
